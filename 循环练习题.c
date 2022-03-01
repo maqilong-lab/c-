56269a2798e7fc0c0fdc7461c2dc00f6a4b081ce
maqilong <84676088+maqilong-lab@users.noreply.github.com>
2022-03-02 00:34:09 +0800

    几个循环练习题代码

---
+++ 循环练习题.c
@@ -0,0 +1,74 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+
+
+//int main()
+//{
+//	int arr[] = {1,2,3,4,5,6,7,8,9,10};
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int m = 0;
+//	int n = 0;
+//	scanf("%d", &m);
+//	for (n = 0; n <= sz; n++)
+//	{
+//		if (m == arr[n])
+//		{
+//			printf("存在%d位於數組第%d個", m, n + 1);
+//			break;
+//		}
+//	}
+//	if (m >= n)
+//		printf("數組中不存在%d", m);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int ret = 1;
+//	int sum = 0;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		ret *= i;
+//		sum += ret;
+//	}
+//	printf("sum = %d\n", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int ret = 1; 
+//	int i = 0;
+//	int n = 5;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret = ret * i;
+//	}
+//	printf("ret = %d\n", ret);
+//
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int ret = 1;
+//	int n = 0;
+//	int i = 1;
+//	scanf("%d", &n);
+//	while (n >= i)
+//	{
+//		ret *= i;
+//		i++;
+//	}
+//	printf("ret = %d", ret);
+//
+//	return 0;
+//}
+
