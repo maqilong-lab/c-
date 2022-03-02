1248dd09971254977293b431ff54bce70d5b351f
maqilong <84676088+maqilong-lab@users.noreply.github.com>
2022-03-02 23:57:30 +0800

    简单的比较大小代码

---
+++ text3-2比较大小.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int temp = a;
+//	scanf("%d%d%d", &a, &b, &c);
+//	if (a < b)
+//	{
+//		temp = a;
+//		a = b;
+//		b = temp;
+//	}
+//	if (a < c)
+//	{
+//		temp = a;
+//		a = c;
+//		c = temp;
+//	}
+//	if (b < c)
+//	{
+//		temp = b;
+//		b = c;
+//		c = temp;
+//	}
+//	printf("%d %d %d", a, b, c);//从大到小排序
+//
+//	return 0;
+//}
